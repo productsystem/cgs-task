3274dd866d7ad9537bd2d04a7ec1584dd2ad7b5d
productsystem <prajitpradeep@gmail.com>
2025-02-03 20:13:50 +0530

    simple clean up, and started work on New Input System based movements

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,21 +1,35 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public float jumpForce = 10f;
     public LayerMask groundLayer;
+    public InputAction playerControls;
 
     private Rigidbody2D rb;
     private bool isGrounded = false;
+    private Vector2 moveDir = Vector2.zero;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
+    void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    void OnDisable()
+    {
+        playerControls.Disable();
+    }
+
     void Update()
     {
+        moveDir = playerControls.ReadValue<Vector2>();
         if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             rb.velocity += new Vector2(0, jumpForce);
@@ -32,7 +46,7 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
-        rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, rb.velocity.y);
+        rb.velocity = new Vector2(moveDir.x * moveSpeed, rb.velocity.y);
         RaycastHit2D ray = Physics2D.Raycast(transform.position,Vector2.down,1.1f , groundLayer);
         if(ray.collider != null)
         {
