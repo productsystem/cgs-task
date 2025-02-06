7cadf2adeea783501cfc36bd8ae58ccdaf88c9e5
productsystem <prajitpradeep@gmail.com>
2025-02-07 01:31:37 +0530

    ground tileset implemented, parallax background implemented

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    public Transform player;
+    public float parallaxSpeed = 0.5f;
+
+    private Vector3 prevPos;
+
+    void Start()
+    {
+        prevPos = player.position;
+    }
+
+    void Update()
+    {
+        transform.position += new Vector3((player.position - prevPos).x * parallaxSpeed,0,0);
+        prevPos = player.position;
+    }
+}
